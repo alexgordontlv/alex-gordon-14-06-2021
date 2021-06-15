72374a25789bb1e70fb5f1b244b8eff41d335caa
alexgordontlv <alexaybn@gmail.com>
2021-06-15 10:50:02 +0300

    drop down

--- src/pages/mainpage/Mainpage.js
+++ src/pages/mainpage/Mainpage.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import Card from '../../components/card/Card';
+import { Combobox, ComboboxInput, ComboboxPopover, ComboboxList, ComboboxOption } from '@reach/combobox';
 import axios from 'axios';
 
 import './mainpage.styles.css';
 import { useQuery } from 'react-query';
-const API = 'KMQOVqRgfYZd7EkMk5e2xVcabmETmQvd';
+const API = 'WJ9kOJdwSpEsVz3E6l5ULWiPpX8JoJL0';
 const Mainpage = () => {
 	const [input, setInput] = useState('');
-	const [suggestions, setSuggestions] = useState('');
-
+	const [suggestions, setSuggestions] = useState([]);
 	const getAutoComplete = async (location) => {
 		const { data } = await axios.get(`http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${API}&q=${location}`);
 		setSuggestions(data);
@@ -23,7 +23,23 @@ const Mainpage = () => {
 	console.log('suggestions', suggestions);
 	return (
 		<div>
-			<input onChange={(e) => setInput(e.target.value)} className='location_input' tpye='text' placeholder='Enter Location' />
+			<div className='search'>
+				<Combobox>
+					<ComboboxInput
+						value={input}
+						onChange={(e) => {
+							setInput(e.target.value);
+						}}
+						placeholder='Search your city weather!'
+					/>
+					<ComboboxPopover>
+						<ComboboxList className='option'>
+							{suggestions &&
+								suggestions.map((city, idx) => <ComboboxOption key={idx} value={city.LocalizedName} className='search_option' />)}
+						</ComboboxList>
+					</ComboboxPopover>
+				</Combobox>
+			</div>
 			<Card weatherCondition='cloudy'>hi</Card>
 		</div>
 	);
