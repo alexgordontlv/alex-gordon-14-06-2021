3fdb4852f726483b1867eca3956f0b9b88bd9a45
alexgordontlv <alexaybn@gmail.com>
2021-06-16 22:21:57 +0300

    axios headers

--- src/utilities/fetchfunction.js
+++ src/utilities/fetchfunction.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 
 const API = 'KMQOVqRgfYZd7EkMk5e2xVcabmETmQvd';
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 const fetchUrls = async (queryKey, cityKey) => {
 	let error;
 	let data;
 	const urls = {
-		autoComplete: `https://cors-anywhere.herokuapp.com/https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${API}&q=${cityKey}`,
-		weatherCondition: `https://cors-anywhere.herokuapp.com/https://dataservice.accuweather.com/currentconditions/v1/${cityKey}?apikey=${API}`,
-		weatherForecast: `https://cors-anywhere.herokuapp.com/https://dataservice.accuweather.com//forecasts/v1/daily/5day/${cityKey}?metric=true&apikey=${API}`,
+		autoComplete: `https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${API}&q=${cityKey}`,
+		weatherCondition: `https://dataservice.accuweather.com/currentconditions/v1/${cityKey}?apikey=${API}`,
+		weatherForecast: `https://dataservice.accuweather.com//forecasts/v1/daily/5day/${cityKey}?metric=true&apikey=${API}`,
 	};
 	try {
 		const res = await axios.get(urls[queryKey]);
